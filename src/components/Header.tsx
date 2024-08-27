ef5920749acde2390346a5170e7286676f40b879
justcovenant <justcovenant@outlook.com>
2024-08-27 18:02:02 +0100

    feat: added the hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo"
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center p-6 bg-white fixed w-full ">
+    <div className="flex justify-between items-center p-6 bg-white sticky w-full">
       <div><Logo /></div>
       <div>
         <button className="bg-main text-white p-3 rounded-lg text-center">Add New Product</button>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+
+const Hero = () => {
+  return (
+    <div className="bg-gradient-to-tr from-[#F4E8F3] via-[#F3EFF6] to-[#EEE0F9] flex justify-between px-16 items-center">
+      <div className="text-main text-4xl font-extrabold">Experience the future of shopping <br /> With us.</div>
+      <div><img src="/images/hero.png" alt="Girl wearing headphones" className="h-full"/></div>
+    </div>
+  )
+}
+
+export default Hero
